77867711f4979804a34f28f382ef12387e299b68
FerVassolo <63514959+Parrandos@users.noreply.github.com>
2024-06-06 20:41:15 -0300

    First commit

---
+++ src/main/java/com/example/snippetpermits/SnippetPermitsApplication.java
@@ -0,0 +1,13 @@
+package com.example.snippetpermits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippetPermitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnippetPermitsApplication.class, args);
+    }
+
+}
