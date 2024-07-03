e5189f2f07ded704fc36e55695547e7a9b8fc198
Franco B. <franco.barravecchia2002@gmail.com>
2024-07-03 20:20:28 -0300

    feat: PermitRespository

---
+++ src/main/java/com/example/snippetpermits/repository/PermitRepository.java
@@ -0,0 +1,8 @@
+package com.example.snippetpermits.repository;
+
+import com.example.snippetpermits.model.Permit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PermitRepository extends JpaRepository<Permit, Long> {
+    Permit findByFileNameAndOwnerIdAndUserId(String fileName, Long ownerId, Long userId);
+}
