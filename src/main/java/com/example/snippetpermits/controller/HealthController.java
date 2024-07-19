63929d645daa4780c05dfa6e3ad7a2d8aace574f
Franco B. <franco.barravecchia2002@gmail.com>
2024-07-19 10:08:23 -0300

    feat: make health visible in infra

--- src/main/java/com/example/snippetpermits/controller/HealthController.java
+++ src/main/java/com/example/snippetpermits/controller/HealthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/health")
+@RequestMapping("/permits/health")
 public class HealthController {
     private static final Logger logger = LoggerFactory.getLogger(HealthController.class);
 
