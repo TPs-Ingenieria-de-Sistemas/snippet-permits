038b29f548b8f6a381483e69afca8d4c68449ccf
Franco B. <franco.barravecchia2002@gmail.com>
2024-07-20 17:13:42 -0300

    fix: remove permits form url

--- src/main/java/com/example/snippetpermits/controller/PermitController.java
+++ src/main/java/com/example/snippetpermits/controller/PermitController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/permits")
+@RequestMapping("/")
 public class PermitController {
     private final PermitService permitService;
     private final UserService userService;
